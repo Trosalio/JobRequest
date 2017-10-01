00064e6d4162c9b78bb11e2fd23283f3bb08591e
Trosalio <thanapong.su@ku.th>
2017-10-02 06:43:19 +0700

    Add Functioning in controllers&fxml: -EmailChanger -Main -Memo -Memomaster Utilities: -AlertBox -DatePickerFormat -DateTimerFormat

--- src/main/java/controllers/EmailChangerController.java
+++ src/main/java/controllers/EmailChangerController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import Utilities.AlertBoxSingleton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * Project Name: MemoView
@@ -12,16 +15,36 @@ public class EmailChangerController {
 
     @FXML
     private TextField receiverTxtF;
+    @FXML
+    private Button cancelButton;
+    private String receiverEmail;
 
     @FXML
-    void onCancel(ActionEvent event) {
-
+    private void onChange() {
+        if(!receiverTxtF.getText().isEmpty()){
+            receiverEmail = receiverTxtF.getText();
+        }
+        AlertBoxSingleton.getInstance().popAlertBox("Success", "Receiver Email is changed!");
+        closeWindow();
     }
 
     @FXML
-    void onChange(ActionEvent event) {
-
+    private void onCancel() {
+        closeWindow();
     }
 
+    private void closeWindow() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void setReceiverTxtF(String receiverEmail){
+        receiverTxtF.setPromptText(receiverEmail);
+        this.receiverEmail = receiverEmail;
+    }
+
+    public String getReceiverEmail(){
+        return receiverEmail;
+    }
 }
 
