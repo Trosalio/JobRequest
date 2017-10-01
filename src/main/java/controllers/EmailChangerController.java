2f164c37e80c72149b4845c2349db0439026a12f
Trosalio <thanapong.su@ku.th>
2017-10-01 17:57:18 +0700

    Added UIs and their Controllers

---
+++ src/main/java/controllers/EmailChangerController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Project Name: MemoView
+ */
+
+public class EmailChangerController {
+
+    @FXML
+    private TextField receiverTxtF;
+
+    @FXML
+    void onCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onChange(ActionEvent event) {
+
+    }
+
+}
+
