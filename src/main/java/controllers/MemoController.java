2db95f07a15122002a43e270156f82f693e42958
Trosalio <thanapong.su@ku.th>
2017-10-02 18:45:14 +0700

    Add Functioning in controllers&fxml: -EmailChanger -Main -Memo -Memomaster Utilities: -AlertBox -DatePickerFormat -DateTimerFormat

--- src/main/java/controllers/MemoController.java
+++ src/main/java/controllers/MemoController.java
@@ -1,39 +1,107 @@
 package controllers;
 
-import javafx.event.ActionEvent;
+import Utilities.AlertBoxSingleton;
+import Utilities.DatePickerFormatSingleton;
+import Utilities.DateTimeFormatSingleton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import models.Memo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Project Name: MemoView
  */
+
 public class MemoController {
 
     @FXML
     private DatePicker sDatePicker;
-
     @FXML
     private DatePicker eDatePicker;
-
     @FXML
     private DatePicker cDatePicker;
-
     @FXML
     private TextField subjectTxtF;
-
     @FXML
     private TextField refNoTxtF;
+    @FXML
+    private Button cancelButton;
+
+    private Boolean saveBool = false;
+    private Memo memo;
 
     @FXML
-    void onCancel(ActionEvent event) {
-
+    private void initialize() {
+        setsDatePickers();
     }
 
     @FXML
-    void onSave(ActionEvent event) {
+    private void onCancel() {
+        closeWindow();
+    }
+
+    @FXML
+    private void onSave() {
+        if (isValidDate()) {
+            if (!(subjectTxtF.getText().isEmpty() || refNoTxtF.getText().isEmpty())) {
+                memo.setCreateMemoDate(cDatePicker.getValue());
+                memo.setMemoName(subjectTxtF.getText());
+                memo.setRefNumber(refNoTxtF.getText());
+                memo.setStartMemoDate(sDatePicker.getValue());
+                memo.setEndMemoDate(eDatePicker.getValue());
+                AlertBoxSingleton.getInstance().popAlertBox("Success", "Memo is saved!");
+                saveBool = true;
+                closeWindow();
+            } else {
+                AlertBoxSingleton.getInstance().popAlertBox("Error", "Subject or Reference Number is not filled");
+                if (refNoTxtF.getText().isEmpty()) {
+                    refNoTxtF.requestFocus();
+                }
+                if (subjectTxtF.getText().isEmpty()) {
+                    subjectTxtF.requestFocus();
+                }
+            }
+        } else {
+            AlertBoxSingleton.getInstance().popAlertBox("Error", "End Date must not end before Start Date");
+            eDatePicker.setValue(sDatePicker.getValue());
+        }
 
     }
 
+    public void setCurrentMemo(Memo currentMemo) {
+        this.memo = currentMemo;
+        cDatePicker.setValue(memo.getCreateMemoDate());
+        subjectTxtF.setText(memo.getMemoName());
+        refNoTxtF.setText(memo.getRefNumber());
+        sDatePicker.setValue(memo.getStartMemoDate());
+        eDatePicker.setValue(memo.getEndMemoDate());
+    }
+
+    private boolean isValidDate() {
+        return !eDatePicker.getValue().isBefore(sDatePicker.getValue());
+    }
+
+    private void setsDatePickers() {
+        DateTimeFormatter dtf = DateTimeFormatSingleton.getInstance().getDateTimeFormat();
+        DatePickerFormatSingleton.getInstance().setDatePickerFormat(cDatePicker, dtf);
+        DatePickerFormatSingleton.getInstance().setDatePickerFormat(sDatePicker, dtf);
+        DatePickerFormatSingleton.getInstance().setDatePickerFormat(eDatePicker, dtf);
+    }
+
+
+    private void closeWindow() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public boolean isSaved() {
+        return saveBool;
+    }
 }
 
--- src/main/java/controllers/MemoMasterController.java
+++ src/main/java/controllers/MemoMasterController.java
@@ -1,9 +1,20 @@
 package controllers;
 
-import javafx.event.ActionEvent;
+import Utilities.DatePickerFormatSingleton;
+import Utilities.DateTimeFormatSingleton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import models.Memo;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 
 
 /**
@@ -14,34 +25,80 @@ public class MemoMasterController {
 
     @FXML
     private TableView<?> formTable;
+    @FXML
+    private Label subjectLabel, refNoLabel, receiverLabel;
+    @FXML
+    private DatePicker cDatePicker, sDatePicker, eDatePicker;
+    private Memo memo;
 
     @FXML
-    private Label receiverLabel;
+    private void intialize() {
+        setDatePickers();
+    }
 
     @FXML
-    void onChangeReceiver(ActionEvent event) {
+    private void onCreateForm() {
 
     }
 
     @FXML
-    void onCreateForm(ActionEvent event) {
+    private void onDelete() {
 
     }
 
     @FXML
-    void onDelete(ActionEvent event) {
+    private void onEdit() {
 
     }
 
     @FXML
-    void onEdit(ActionEvent event) {
+    private void onSend() {
 
     }
 
     @FXML
-    void onSend(ActionEvent event) {
-
+    private void onChangeReceiver() {
+        String receiverEmail = popEmailChangerWindow();
+        if (receiverEmail != null) {
+            receiverLabel.setText(receiverEmail);
+        }
     }
 
+    private void setDatePickers() {
+        DateTimeFormatter dtf = DateTimeFormatSingleton.getInstance().getDateTimeFormat();
+        DatePickerFormatSingleton.getInstance().setDatePickerFormat(cDatePicker, dtf);
+        DatePickerFormatSingleton.getInstance().setDatePickerFormat(sDatePicker, dtf);
+        DatePickerFormatSingleton.getInstance().setDatePickerFormat(eDatePicker, dtf);
+    }
+
+
+    private String popEmailChangerWindow() {
+        try {
+            FXMLLoader emailChangerUILoader = new FXMLLoader(getClass().getResource("/fxml/EmailChangerUI.fxml"));
+            Parent root = emailChangerUILoader.load();
+            EmailChangerController emailChangerController = emailChangerUILoader.getController();
+            emailChangerController.setReceiverTxtF(receiverLabel.getText());
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Changing E-mail");
+            stage.setResizable(false);
+            stage.showAndWait();
+            return emailChangerController.getReceiverEmail();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void setCurrentMemo(Memo currentMemo) {
+        this.memo = currentMemo;
+        subjectLabel.setText(memo.getMemoName());
+        refNoLabel.setText(memo.getRefNumber());
+        cDatePicker.setValue(memo.getCreateMemoDate());
+        sDatePicker.setValue(memo.getStartMemoDate());
+        eDatePicker.setValue(memo.getEndMemoDate());
+    }
 }
 
